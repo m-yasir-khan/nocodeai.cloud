ed004cf1a9c070bfccdae6a945867b1266883951
m-yasir-khan <yasir.khan@mach3bi.com>
2022-05-31 12:36:37 +0500

    updates

---
+++ client/src/Screens/BPM/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../Components/Header";
+import BPM from '../../Components/BPM';
+// import Footer from "../Footer";
+
+export default function BPMFlow() {
+
+  return (
+    <>
+
+      <Header /> 
+      <Header /> 
+      <Header /> 
+      <BPM />
+      {/* <Footer /> */}
+    </>
+  );
+}
