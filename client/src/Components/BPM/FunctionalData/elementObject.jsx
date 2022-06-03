a8d506f3a9ed7aeee2249ef3b2deec08ced736ea
Ursa Nadeem <ursanadeem13@gmail.com>
2022-06-03 14:43:49 +0500

    BPM/usecases

--- client/src/Components/BPM/FunctionalData/elementObject.jsx
+++ client/src/Components/BPM/FunctionalData/elementObject.jsx
@@ -1,62 +1,193 @@
-import {
-    InputText,
-    HTML,
-    Kanban
-} from '../Nodes'
+import { InputText, HTML, Kanban, MaterialReciept, Withdrawal, KaizenBurst,
+   TruckMovement, GoSee, ControlledFifo, Inventory, PushSystem, Inbox, FaxMachine} from "../Nodes";
 
-const handleElements = (getType, position, getId, dataProperties, dataDelete) => {
-    const inputField = {
-        component: InputText,
-        type: getType,
-        selected: false,
-        position,
-        width: 200,
-        height: 40,
-        properties: dataProperties,
-        delete: dataDelete,
-        id: getId,
-        label: "Default",
-        name: "Default",
-        inputTypes: [{ id: "0", type: "Text" }, { id: "1", type: "Integer" }, { id: "2", type: "Float" }, { id: "3", type: "Date" }, { id: "4", type: "Time" }, { id: "5", type: "Email" }, { id: "6", type: "Currency" }, { id: "7", type: "Signature" }, { id: "8", type: "URL" }, { id: "9", type: "Calculated Field" }, { id: "10", type: "Password" }, { id: "11", type: "Phone Number" }, { id: "12", type: "Keys" }, { id: "13", type: "Address" }]
-    }
+const handleElements = (
+  getType,
+  position,
+  getId,
+  dataProperties,
+  dataDelete
+) => {
+  const inputField = {
+    component: InputText,
+    type: getType,
+    selected: false,
+    position,
+    width: 200,
+    height: 40,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+    inputTypes: [
+      { id: "0", type: "Text" },
+      { id: "1", type: "Integer" },
+      { id: "2", type: "Float" },
+      { id: "3", type: "Date" },
+      { id: "4", type: "Time" },
+      { id: "5", type: "Email" },
+      { id: "6", type: "Currency" },
+      { id: "7", type: "Signature" },
+      { id: "8", type: "URL" },
+      { id: "9", type: "Calculated Field" },
+      { id: "10", type: "Password" },
+      { id: "11", type: "Phone Number" },
+      { id: "12", type: "Keys" },
+      { id: "13", type: "Address" },
+    ],
+  };
 
- 
+  const html = {
+    component: HTML,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+    code: "<p>Write HTML code here!</p>",
+  };
+  const kanban = {
+    component: Kanban,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const materialReciept = {
+    component: MaterialReciept,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const withdrawal = {
+    component: Withdrawal ,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const kaizenBurst= {
+    component: KaizenBurst ,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const truckMovement= {
+    component: TruckMovement ,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const goSee= {
+    component: GoSee ,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const controlledFifo= {
+    component: ControlledFifo ,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const inventory= {
+    component: Inventory ,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const pushSystem= {
+    component: PushSystem ,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const inbox= {
+    component: Inbox ,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const faxMachine= {
+    component: FaxMachine ,
+    type: getType,
+    selected: false,
+    position,
+    properties: dataProperties,
+    delete: dataDelete,
+    id: getId,
+    label: "Default",
+    name: "Default",
+  };
+  const Components = {
+    inputfield: inputField,
+    html,
+    kanban,
+    materialReciept,
+    withdrawal,
+    kaizenBurst,
+    truckMovement,
+    goSee,
+    controlledFifo,
+    inventory,
+    pushSystem,
+    inbox,
+    faxMachine
+  };
 
-    const html = {
-        component: HTML,
-        type: getType,
-        selected: false,
-        position,
-        properties: dataProperties,
-        delete: dataDelete,
-        id: getId,
-        label: "Default",
-        name: "Default",
-        code: "<p>Write HTML code here!</p>"
-
-    }
-    const kanban = {
-        component: Kanban,
-        type: getType,
-        selected: false,
-        position,
-        properties: dataProperties,
-        delete: dataDelete,
-        id: getId,
-        label: "Default",
-        name: "Default",
-
-    }
-
-
-    const Components = {
-        inputfield: inputField,
-        html,
-        kanban
-    }
-
-    return Components[getType]
-
-}
+  return Components[getType];
+};
 
 export default handleElements;
--- client/src/Components/BPM/Nodes/MaterialReciept/index.jsx
+++ client/src/Components/BPM/Nodes/MaterialReciept/index.jsx
@@ -1,36 +1,140 @@
-import './style.css';
-import Tooltip from '../Tooltip';
-import { constants } from '../../../../Utils/constants'
+import "./style.css";
+import Tooltip from "../Tooltip";
+import { useState, useEffect } from 'react';
+import { constants } from "../../../../Utils/constants";
+import { Rnd } from "react-rnd";
+import ResizeHandle from "../resizeHandle";
+import { flagBottom, flagTop, flagLeft, flagRight } from '../nodesFlag';
+import {
+  copyNode,
+  handleSelectNode,
+  handleUpdateNode,
+  selectMultipleNodes,
+} from "../../FunctionalData";
+function MaterialReciept(props) {
+  let data = props?.data;
+  data.type = "materialReciept";
 
-function Merge(props) {
-     let data = props?.data;
-    // data.type = "swimlane";
+  const [multiple, setMultiple] = useState(false);
+  const [fired, setFired] = useState(false);
+  const [width, setWidth] = useState(data?.width);
+  const [height, setHeight] = useState(data?.height);
 
-    const onClickSetting = () => {
-         props?.properties(data);
-    }
-
-    const onClickDelete = () => {
-         props?.delete(data);
-    }
-
-    let element = (
-        <div className="Merge">
-        <p>{data.label}</p>
-         </div>
-    )
-
-    const iconStyle = { height: "23px", width: "23px", marginTop: "4px", marginLeft: "4px", marginRight: "4px", cursor: "pointer" };
-
-    return (
-        <div style={{width: "100%", minHeight: "98px", display: "flex", margin: "auto", height: "100%"}} onClick={onClickSetting}>
-            <Tooltip element={element} tooltip={
-            <div style={{ display: "block" }}>
-                <constants.AiOutlineSetting onClick={onClickSetting} style={iconStyle} />
-                <constants.AiOutlineDelete onClick={onClickDelete} style={iconStyle} />
-            </div>} />
-        </div>
+  const onClickSetting = (event) => {
+    data?.properties(
+      event,
+      data,
+      props?.setNodeProperty,
+      props?.setPropertyVisible
     );
+  };
+
+  const onClickDelete = (event) => {
+    data?.delete(event, data, props?.elements, props?.setElements);
+  };
+
+  const handleClick = (event) => {
+    multiple
+      ? selectMultipleNodes(
+          event,
+          data?.id,
+          props?.elements,
+          props?.setElements
+        )
+      : handleSelectNode(event, data?.id, props?.elements, props?.setElements);
+  };
+  const style = {
+    color: "#333",
+    border: "1px solid #dedede",
+    display: "block",
+    borderRadius: "1px",
+    backgroundColor: "#f9f9f9",
+    width: "100%",
+    padding: "30px"
+}
+const handleComponent = {
+    topRight: data?.selected ? <ResizeHandle /> : "",
+    topLeft: data?.selected ? <ResizeHandle /> : "",
+    bottomLeft: data?.selected ? <ResizeHandle /> : "",
+    bottomRight: data?.selected ? <ResizeHandle /> : ""
+}
+const handleStyles = {
+    top: data?.selected
+        ? flagTop
+        : "",
+    left: data?.selected
+        ? flagLeft
+        : "",
+    bottom: data?.selected && flagBottom,
+    right: data?.selected
+        ? flagRight
+        : ""
 }
 
-export default Merge;
+  let element = (
+    <Rnd
+      style={style}
+      size={{ width: width, height: height }}
+      resizeHandleComponent={handleComponent}
+      resizeHandleStyles={handleStyles}
+      minWidth={data?.minWidth}
+      minHeight={data?.minHeight}
+      maxHeight={data?.maxHeight}
+      maxWidth={data?.maxWidth}
+      position={{ x: 10, y: 10 }}
+      className="bg_kanban"
+      onDragStop={(e, d) => {
+        // setX1(d.x); setY1(d.y);
+      }}
+      onDrag={(e, d) => {}}
+      onResizeStop={(e, direction, ref, delta, position) => {
+        setWidth(ref.style.width);
+        setHeight(ref.style.height);
+      }}
+    >
+      <div className="Merge">
+        <p>{data.label}</p>
+      </div>
+    </Rnd>
+  );
+
+  const iconStyle = {
+    height: "23px",
+    width: "23px",
+    marginTop: "4px",
+    marginLeft: "4px",
+    marginRight: "4px",
+    cursor: "pointer",
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        minHeight: "98px",
+        display: "flex",
+        margin: "auto",
+        height: "100%",
+      }}
+      onClick={handleClick}
+    >
+      <Tooltip
+        element={element}
+        tooltip={
+          <div style={{ display: "block" }}>
+            <constants.AiOutlineSetting
+              onClick={onClickSetting}
+              style={iconStyle}
+            />
+            <constants.AiOutlineDelete
+              onClick={onClickDelete}
+              style={iconStyle}
+            />
+          </div>
+        }
+      />
+    </div>
+  );
+}
+
+export default MaterialReciept;
